83959c765b1c5d1a51e330febc50a91de191edab
clevisonbarbosa <barbosaclevison@gmail.com>
2022-12-04 11:31:18 -0300

    first commit

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,67 @@
+using System.Net;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Controller]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        private DataContext dc;
+        public PessoaController(DataContext context)
+        {
+            this.dc = context;
+        }
+
+        [HttpPost("api")]
+        public async Task<ActionResult> cadastrar([FromBody] Pessoa p)
+        {
+            dc.pessoa.Add(p);
+            await dc.SaveChangesAsync();
+
+            return Created("Objeto pessoa", p);
+        }
+
+        [HttpGet("api")]
+        public async Task<ActionResult> listar()
+        {
+            var dados = await dc.pessoa.ToListAsync();
+            return Ok(dados);
+        }
+
+        [HttpGet("api/{codigo}")]
+        public Pessoa filtrar(int codigo)
+        {
+            Pessoa p = dc.pessoa.Find(codigo);
+            return p;
+        }
+
+        [HttpPut("api")]
+        public async Task<ActionResult> editar([FromBody] Pessoa p)
+        {
+            dc.pessoa.Update(p);
+            await dc.SaveChangesAsync();
+
+            return Ok(p); 
+        }
+
+        [HttpDelete("api/{codigo}")]
+        public async Task<ActionResult> remover(int codigo)
+        {
+            Pessoa p =  filtrar(codigo);
+            if (p == null)
+            {
+                return NotFound();
+            }else
+            {
+                dc.pessoa.Remove(p);
+                await dc.SaveChangesAsync();
+                return Ok();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+[Api]
+
+- using MySQL database;
+- extension for testing Thunder Client endpoints;
+- use of asynchronous methods;
+- with GET, POST, PUT, DELETE requests;
